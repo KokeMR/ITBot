4c022f77eb3e0b819949a6aa15f1c9f128254490
Jorge de la Morena Rivas (Adavan Sistemas S.L.) <v-jorde@microsoft.com>
2017-02-21 16:23:35 +0100

    user+pw added

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var builder = require('botbuilder'),
-restify = require('restify');
+    restify = require('restify');
 
 //restify 
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: 'e0b2dae7-0092-4fe1-b722-b7644af13c3f',
+    appPassword: 'OaTjdzfJfeOSyMXmskNwOX4'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
