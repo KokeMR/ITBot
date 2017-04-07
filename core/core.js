2d184eb85ab1f1f91efaf52b9da47a7ce5d04a4a
Jorge de la Morena Rivas (Adavan Sistemas S.L.) <v-jorde@microsoft.com>
2017-04-07 13:50:57 +0200

    set up back to original

--- core/core.js
+++ core/core.js
@@ -18,7 +18,7 @@ exports.showChoices = (session, results) => {
                 .buttons([
                     
                     builder.CardAction.openUrl(session, util.format('mailto:%s?Subject=BotIT:', element.mail), 'Email'),
-                    builder.CardAction.openUrl(session, util.format('skype:v-juanmc@microsoft.com?chat', element.mail), 'Skype')
+                    builder.CardAction.openUrl(session, util.format('skype:%s?chat', element.alias), 'Skype')
 
                 ])
         );
