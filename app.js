bd5ffe3a1593cd620ebab55e62659ca5adb8503c
Jorge de la Morena Rivas (Adavan Sistemas S.L.) <v-jorde@microsoft.com>
2017-04-08 15:21:46 +0200

    removed id from code

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var builder = require('botbuilder'),
     restify = require('restify'),
     recognizer = new builder.LuisRecognizer(process.env.LUIS_URL),
-        //'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/19ef6460-9e63-4df8-b272-bc65d4f71e88?subscription-key=67936a6d4c134618abde8052836fbec3&verbose=true&q='),
     intents = new builder.IntentDialog({ recognizers: [recognizer] }),   
     core = require('./core/core');
 
@@ -17,8 +16,6 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.Microsoft_AppPassword
     
 });
- //'e0b2dae7-0092-4fe1-b722-b7644af13c3f',
- //'OaTjdzfJfeOSyMXmskNwOX4'
 var bot = new builder.UniversalBot(connector);
 
 //static files
