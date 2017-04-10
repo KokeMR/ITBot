467e06b174bc4176769b99116a76fb8aeafa55fc
Jorge de la Morena Rivas (Adavan Sistemas S.L.) <v-jorde@microsoft.com>
2017-04-10 13:22:28 +0200

    .

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var builder = require('botbuilder'),
     restify = require('restify'),
-    recognizer = new builder.LuisRecognizer("https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/19ef6460-9e63-4df8-b272-bc65d4f71e88?subscription-key=67936a6d4c134618abde8052836fbec3&timezoneOffset=0.0&verbose=false&q="),
+    recognizer = new builder.LuisRecognizer("https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/19ef6460-9e63-4df8-b272-bc65d4f71e88?subscription-key=67936a6d4c134618abde8052836fbec3&verbose=false&q="),
     //recognizerES = new builder.LuisRecognizer(process.env.LUIS_ES_URL' = https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/0969a044-ea94-4a24-82a5-6323fab6782e?subscription-key=67936a6d4c134618abde8052836fbec3&timezoneOffset=1.0&verbose=true&q=),
     //intents = new builder.IntentDialog({ recognizers: [recognizer, recognizerES] 
     intents = new builder.IntentDialog({ recognizers: [recognizer] }),   
