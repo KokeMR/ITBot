172b10a631f042d76f804ca66ddd673d6afc4af9
Jorge de la Morena Rivas (Adavan Sistemas S.L.) <v-jorde@microsoft.com>
2017-04-11 10:21:39 +0200

    .

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var builder = require('botbuilder'),
     restify = require('restify'),
-    recognizer = new builder.LuisRecognizer(/*process.env.LUIS_URL)*/'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/19ef6460-9e63-4df8-b272-bc65d4f71e88?subscription-key=63e6b42f2f3c4531870c89df31430db1&timezoneOffset=0.0&verbose=true&q='),
+    recognizer = new builder.LuisRecognizer(/*process.env.LUIS_URL)*/'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/19ef6460-9e63-4df8-b272-bc65d4f71e88?subscription-key=b1012219ddd4429e9185dc920fd83107&timezoneOffset=0.0&verbose=true&q='),
     //recognizerES = new builder.LuisRecognizer(process.env.LUIS_ES_URL' = https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/0969a044-ea94-4a24-82a5-6323fab6782e?subscription-key=67936a6d4c134618abde8052836fbec3&timezoneOffset=1.0&verbose=true&q=),
     //intents = new builder.IntentDialog({ recognizers: [recognizer, recognizerES] 
     intents = new builder.IntentDialog({ recognizers: [recognizer] }),   
@@ -14,8 +14,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 //Chat connector
 var connector = new builder.ChatConnector({
-    appId: /*process.env.Microsoft_AppId*/'e0b2dae7-0092-4fe1-b722-b7644af13c3f',
-    appPassword: /*process.env.Microsoft_AppPassword*/'OaTjdzfJfeOSyMXmskNwOX4'
+    appId: process.env.Microsoft_AppId,//'e0b2dae7-0092-4fe1-b722-b7644af13c3f',
+    appPassword: process.env.Microsoft_AppPassword//'OaTjdzfJfeOSyMXmskNwOX4'
     
 });
 var bot = new builder.UniversalBot(connector);
