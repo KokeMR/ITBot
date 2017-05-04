0c7efb58f9efdd6f4c9e53255bebb121b6f0e756
Jorge de la Morena Rivas (Adavan Sistemas S.L.) <v-jorde@microsoft.com>
2017-05-04 15:52:09 +0200

    .

--- dialogs/help.js
+++ dialogs/help.js
@@ -1,15 +1,16 @@
 const
     builder = require('botbuilder'),
     core = require('../core/core'),
-    helpType = require('../data/helptype');
+    helpType = require('../data/helptype'),
+    helpTypeES = require('../data/helptypeES')
 
 module.exports = [
     function (session) {
-        builder.Prompts.choice(session, "helptype_prompt", helpType, { listStyle: builder.ListStyle["button"] });
+        builder.Prompts.choice(session, "helptype_prompt", "helpTypevariable", { listStyle: builder.ListStyle["button"] });
     },
     function (session, results) {
         if (results.response) {
-            var region = helpType[results.response.entity];
+            var region = "helptypevariable"[results.response.entity];
             console.log("region", region);
 
             session.send("contact");
